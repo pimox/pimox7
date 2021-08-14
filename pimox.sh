d2078796bc6095a621f1fb8eb5af692bf137cb56
tim <tim.j.wilkinson@gmail.com>
2021-08-14 05:38:29 +0000

    Updae

---
+++ pimox.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+
+curl https://gitlab.com/pimox/pimox7/-/raw/master/KEY.gpg | apt-key add -
+echo "deb http://gitlab.home/pimox/pimox7/-/raw/master/ dev/" > /etc/apt/sources.list.d/pimox.list
+echo "deb http://deb.debian.org/debian bullseye contrib" > /etc/apt/sources.list.d/buster-contrib.list
+apt update
+apt install proxmox-ve
