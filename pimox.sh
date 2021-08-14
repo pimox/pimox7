09cf5251ef1fc264afcd931c6f6d999748d7abca
tim <tim.j.wilkinson@gmail.com>
2021-08-14 05:44:02 +0000

    Update

--- pimox.sh
+++ pimox.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-curl https://gitlab.home/pimox/pimox7/-/raw/master/KEY.gpg | apt-key add -
+curl http://gitlab.home/pimox/pimox7/-/raw/master/KEY.gpg | apt-key add -
 echo "deb http://gitlab.home/pimox/pimox7/-/raw/master/ dev/" > /etc/apt/sources.list.d/pimox.list
 echo "deb http://deb.debian.org/debian bullseye contrib" > /etc/apt/sources.list.d/buster-contrib.list
 apt update
