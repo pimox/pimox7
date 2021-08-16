0a7206c6258071e49b41f5d504754f3d323ce409
tim <tim.j.wilkinson@gmail.com>
2021-08-16 23:32:28 +0000

    Preinstall gnugp

--- pimox.sh
+++ pimox.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
 
+apt install -y gnupg
 curl https://raw.githubusercontent.com/pimox/pimox7/master/KEY.gpg | apt-key add -
 echo "deb https://raw.githubusercontent.com/pimox/pimox7/master/ dev/" > /etc/apt/sources.list.d/pimox.list
 echo "deb http://deb.debian.org/debian bullseye contrib" > /etc/apt/sources.list.d/buster-contrib.list
