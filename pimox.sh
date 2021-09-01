13e94034177b51b1e9e6db9ca8ef1e8be781f9cb
vonloschz <58495670+vonloschz@users.noreply.github.com>
2021-09-01 08:37:05 -0400

    Update pimox.sh
    W: Target Packages (contrib/binary-arm64/Packages) is configured multiple times in /etc/apt/sources.list:1 and /etc/apt/sources.list.d/buster-contrib.list:1
    W: Target Packages (contrib/binary-all/Packages) is configured multiple times in /etc/apt/sources.list:1 and /etc/apt/sources.list.d/buster-contrib.list:1

--- pimox.sh
+++ pimox.sh
@@ -3,6 +3,5 @@
 apt install -y gnupg
 curl https://raw.githubusercontent.com/pimox/pimox7/master/KEY.gpg | apt-key add -
 echo "deb https://raw.githubusercontent.com/pimox/pimox7/master/ dev/" > /etc/apt/sources.list.d/pimox.list
-echo "deb http://deb.debian.org/debian bullseye contrib" > /etc/apt/sources.list.d/buster-contrib.list
 apt update
 apt install -y proxmox-ve
